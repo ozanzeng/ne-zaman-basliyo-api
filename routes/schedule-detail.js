43acf9afde26989d99e3892d20bfb05893893731
Ozan Zengin <oznzengin@gmail.com>
2019-03-19 14:55:19 +0300

    added schedule-detail

--- routes/schedule-detail.js
+++ routes/schedule-detail.js
@@ -7,7 +7,7 @@ const ScheduleDetail = require('../models/ScheduleDetail');
 
 // Get schedule detail
 router.get('/schedule-detail/:schedule_detail_id', (req, res) => {
-  const promise = Schedule.findOne(req.params.schedule_detail_id);
+  const promise = Schedule.findById(req.params._id);
 
   promise.then(data => {
     if (!data) {
