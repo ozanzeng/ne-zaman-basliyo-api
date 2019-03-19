cd99a31c94b0c9148b2a51976857bf9ba078f559
Ozan Zengin <oznzengin@gmail.com>
2019-03-19 15:37:04 +0300

    bcrypt added for password

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
+//Models
 const User = require('../models/User');
-
-
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
+/* POST register form. */
 router.post('/register', (req, res, next) => {
   const { username, password } = req.body;
-  const user = new User({
-    username,
-    password
-  });
 
-  const promise = user.save();
-  promise.then(data => {
-    res.json(data);
-  }).catch(err => {
-    res.json(err);
+  bcrypt.hash(password, 10).then(function(hash) {
+    const user = new User({
+      username,
+      password: hash
+    });
+  
+    const promise = user.save();
+    promise.then(data => {
+      res.json(data);
+    }).catch(err => {
+      res.json(err);
+    });
   });
 });
 
