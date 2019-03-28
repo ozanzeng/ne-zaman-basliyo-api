21f9324fdae3a89d158144148e760d7f1a45c5a3
Ozan Zengin <oznzengin@gmail.com>
2019-03-28 16:26:41 +0300

    AllowedAccess-Control-Allow-Origin updated

--- app.js
+++ app.js
@@ -38,8 +38,23 @@ app.use('/web-api/schedule', scheduleRouter);
 app.use('/web-api/schedule-detail', scheduleDetailRouter);
 app.use('/web-api/category', categoryRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect exp: http://localhost:8888
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // catch 404 and forward to error handler and pass to next layer of middleware
   next(createError(404));
 });
 
